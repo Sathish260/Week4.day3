73286f596cd9a836519a34ac9eb5f11578b92fe2
Sathish260 <132818664+Sathish260@users.noreply.github.com>
2023-05-25 08:44:16 -0700

    Update FacebookLogin.java

--- FacebookLogin.java
+++ FacebookLogin.java
@@ -1,5 +1,7 @@
 package week3;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,32 +9,41 @@ import org.openqa.selenium.support.ui.Select;
 
 public class FacebookLogin {
 
-	public static void main(String[] args) throws InterruptedException {
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws InterruptedException {   // Step 1: Download and set the path
+		// Step 2: Launch the chromebrowser
 		ChromeDriver driver = new ChromeDriver();
-        driver.get("https://en-gb.facebook.com/");
+		// Step 3: Load the URL https://en-gb.facebook.com/
+		driver.get("https://en-gb.facebook.com/");
+		// Step 4: Maximize the window
 		driver.manage().window().maximize();
-		WebElement newAccount = driver.findElement(By.id("//data-testid='open-registration-form-button'"));
-		newAccount.click();
-		WebElement firstName = driver.findElement(By.id("//input[@placeholder aria-label='First name']"));
-		firstName.sendKeys("Ram");
-		WebElement surName = driver.findElement(By.id("//input[@placeholder aria-label='Surname']"));
-		surName.sendKeys("Kumar");
-		WebElement mobile = driver.findElement(By.name("Mobile.number"));
-		mobile.sendKeys("1234567890");
-		WebElement password = driver.findElement(By.id("password_step_input"));
-		password.sendKeys("mar26");
-		WebElement day = driver.findElement(By.name("//select[@title='Day']"));
+		// Step 5: Add implicit wait
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		// Step 6: Click on Create New Account button
+		WebElement newAccount = driver.findElement(By.xpath("//a[@data-testid='open-registration-form-button']")).click();
+		// Step 7: Enter the first name
+		WebElement firstName = driver.findElement(By.xpath("//input[@aria-label='First name']")).sendKeys("Ram");
+		// Step 8: Enter the last name
+		WebElement surName = driver.findElement(By.xpath("//input[@aria-label='Surname']")).sendKeys("kumar");
+		// Step 9: Enter the mobile number
+		WebElement mobile = driver.findElement(By.name("reg_email__")).sendKeys("123456767");
+		// Step 10: Enter the password
+		WebElement password = driver.findElement(By.id("password_step_input")).sendKeys("ram262");
+		// Step 11: Handle all the three drop downs
+		WebElement day = driver.findElement(By.xpath("//select[@title='Day']"));
 		Select dayInput = new Select(day);
-		dayInput.selectByValue("29");
-		WebElement month = driver.findElement(By.name("//select[@name='birthday_month']"));
+		dayInput.selectByValue("26");
+		WebElement month = driver.findElement(By.xpath("//select[@name='birthday_month']"));
 		Select monthInput = new Select(month);
-		monthInput.selectByIndex(6);
-		WebElement year = driver.findElement(By.id("//select[@id='year']"));
+		monthInput.selectByIndex(9);
+		WebElement year = driver.findElement(By.xpath("//select[@id='year']"));
 		Select yearInput = new Select(year);
 		yearInput.selectByVisibleText("2000");
-		WebElement radioButton = driver.findElement(By.id("//input[@value='2']"));
+		// Step 12: Select the radio button "male"
+		WebElement radioButton = driver.findElement(By.xpath("//input[@value='4']"));
+		// ( A normal click will do for this step)
 		radioButton.click();
-		Thread.sleep(10000);
+		Thread.sleep(50000);
 		driver.close();
 	}
 
